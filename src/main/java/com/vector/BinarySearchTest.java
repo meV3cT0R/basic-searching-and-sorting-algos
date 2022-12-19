eeca1681d94ff356ca0891db2807ce4fe4421782
meV3cT0R <shresthasumit09876@gmail.com>
2022-12-19 09:33:56 +0000

    binary search

---
+++ src/main/java/com/vector/BinarySearchTest.java
@@ -0,0 +1,50 @@
+package com.vector;
+
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BinarySearchTest {
+    public static int binarySearch(int[] datas,int key) {
+        int low=0;
+        int high = datas.length -1;
+        int middle = (low+high)/2;
+
+        while(high>=low) {
+
+            if(datas[middle]==key)
+                return middle;
+            
+            else if(key < datas[middle])
+                high = middle-1;
+            else 
+                low = middle+1;
+            
+            middle = (low+high)/2;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        SecureRandom random = new SecureRandom();
+        int[] datas = new int[10];
+        for(int i=0;i<10;i++) 
+            datas[i]= 10+ random.nextInt(120);
+
+        Arrays.sort(datas);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("[ ");
+        for(int data : datas)
+            System.out.printf("%d ",data);
+        System.out.printf("]");
+        
+        System.out.println();
+        System.out.print("Enter a number to search:");
+        int searchKey = scanner.nextInt();
+
+        System.out.println("Index of key : " + binarySearch(datas, searchKey));
+        scanner.close();
+    }
+}
