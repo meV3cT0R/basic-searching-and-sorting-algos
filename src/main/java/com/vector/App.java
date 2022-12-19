f56b2d298f9491a0849ae8f71206ed7ef4a2ffcb
meV3cT0R <shresthasumit09876@gmail.com>
2022-12-19 09:14:39 +0000

    linear search

---
+++ src/main/java/com/vector/App.java
@@ -0,0 +1,13 @@
+package com.vector;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/vector/LinearSearchTest.java
@@ -0,0 +1,34 @@
+package com.vector;
+
+import java.security.SecureRandom;
+import java.util.Scanner;
+
+public class LinearSearchTest {
+    public static int linearSearch(int[] data,int searchKey) {
+        for(int i =0;i<data.length;i++) {
+            if(data[i]==searchKey)
+                return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        SecureRandom random = new SecureRandom();
+        int[] datas = new int[10];
+        for(int i=0;i<10;i++) 
+            datas[i]= 10+ random.nextInt(120);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("[ ");
+        for(int data : datas)
+            System.out.printf("%d ",data);
+        System.out.printf("]");
+        
+        System.out.println();
+        System.out.print("Enter a number to search:");
+        int searchKey = scanner.nextInt();
+
+        System.out.println("Index of key : " + linearSearch(datas, searchKey));
+        scanner.close();
+    }
+}
